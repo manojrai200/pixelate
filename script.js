2e6267d7286b34e6a11a249f27f3a03c1c904ad4
manojrai200 <manojrai9801@gmail.com>
2025-07-22 12:51:35 +0545

    commit

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ colorSelect.addEventListener("change", (e) => {
 // Add a row with current column count
 function makeRow(colCount = getColCount()) {
   const row = document.createElement("tr");
-  for (let i = 0; i < colCount; i++) {
+  for (let i = 0; i < 20; i++) {
     const td = document.createElement("td");
     row.appendChild(td);
   }
